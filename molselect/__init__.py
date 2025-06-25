7a203960467f8844a29bca8ce0fcf82c3af040a1
Carlos Bueno <cabb99@gmail.com>
2025-06-25 00:15:28 -0500

    Adds init files

--- molselect/__init__.py
+++ molselect/__init__.py
@@ -1,7 +1,5 @@
 """A cross-platform extensible parser for VMD-like molecular selections"""
 
 # Add imports here
-from .molselect import *
-
-
+from .python import *
 from ._version import __version__
