71af05bb42e04d932190f6a210a4e773a3102fbf
Carlos Bueno <cabb99@gmail.com>
2026-04-03 08:54:47 -0500

    Update developer guide and simple selection example

--- docs/developer_guide.rst
+++ docs/developer_guide.rst
@@ -7,6 +7,11 @@ TODO
 ----
 [ ] Add test for recursive macro expansion.
 [ ] Add handling for non default units
+[ ] Use prody residue conventions for aliphatic, cyclic, etc including non-native aminoacids
+[ ] Add methods to compute secondary structure
+[ ] Add ability to handle "_" as a synonym for ""
+[ ] Add ability to handle nan values in equations with comparisons
+
 
 
 
--- examples/simple_selection.py
+++ examples/simple_selection.py
@@ -12,13 +12,29 @@ df = molscene.Scene.from_pdb('molselect/data/tests/pdb3mht.pdb')
 
 # Create the evaluator with the backend class
 selector = Evaluator(PandasStructure)
+#selector.parser = molselect.python.parser.SelectionParser(parser_kwargs={'parser': 'earley', 'ambiguity': 'explicit'}, remove_hidden_tokens=True)
 
-# Use the new parse method for simple selection
-result = selector.parse(df, 'hetatm')
-
-# Print the resulting DataFrame
-print(result.df)
+selection_string = "backbone"
 
 #Write the parser grammar to text for debugging purposes
-with open("parser_grammar.txt", "w") as f:
+with open("parser_grammar.lark", "w") as f:
     f.write(selector.parser.grammar)
+
+parser = selector.parser
+forest = parser.parse(selection_string)
+
+# 'forest' is actually a Tree('_ambig', [tree1, tree2, …])
+print(forest.pretty())
+
+# to see them separately:
+for idx, subtree in enumerate(forest.children, 1):
+    print(f"\n=== Parse #{idx} ===")
+    print(subtree.pretty())
+
+# # Use the new parse method for simple selection
+result = selector.parse(df, "altloc ''")
+
+# # Print the resulting DataFrame
+print(result.df)
+
+
